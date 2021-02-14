9fa91df2f0c1ee9caf36206048c2f4e7815d68ab
Zafer Aslan <zafer.aslan@ogr.dpu.edu.tr>
2021-02-14 20:16:56 +0300

    Ders4Odev5

--- DictionaryDemo/Program.cs
+++ DictionaryDemo/Program.cs
@@ -6,7 +6,18 @@ namespace DictionaryDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1699, "Zafer");
+            myDictionary.Add(1696, "Abdullah");
+            myDictionary.Add(1700,"Bora");
+            myDictionary.Add(1703,"Efecan");
+
+            myDictionary.Key(1699);
+            myDictionary.Key(1710);
+            myDictionary.Key(1696);
+
+            Console.ReadLine();
+
         }
     }
 }
